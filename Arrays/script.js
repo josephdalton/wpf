d6d3573b68c83c01973d1b9842d4718970e7c665
Harmony Cornett <harmony@fullsail.edu>
2013-10-12 08:09:45 -0400

    initial commit

---
+++ Arrays/script.js
@@ -0,0 +1,12 @@
+// Arrays
+
+var muppetNames=["Kermit", "Miss Piggy", "Gonzo", "Rizzo"]; //0,1,2,3
+
+console.log(muppetNames[1]); //muppetNames[1]="Rizzo";
+
+
+muppetNames[1]); //lets me see what's in the index #1
+
+var num=2;
+
+console.log(muppetNames[num]); 
