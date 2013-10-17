88c6108f70bf023d544b54fb72ce4875fe815728
Harmony Cornett <harmony@fullsail.edu>
2013-10-17 19:09:21 -0400

    created the example

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -2,5 +2,8 @@ Harmony Cornett 10/17/2003
 
 Conditionals_Wacky
 
-Example:
+//The dog years, new research.
+//This asks, how old is your dog.  The difference (from the Sparky example from earlier videos) is that new research suggests that the actual age is multiplied by 7, UNTIL they get to a certain age.
 
+var dogAge = ;
+var dogAgeHumanYears = dogAge * 7;
