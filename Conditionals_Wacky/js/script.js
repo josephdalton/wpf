a909c9560267136267e7a3e9ba0d9c57c0fb762e
Harmony Cornett <harmony@fullsail.edu>
2013-10-17 19:33:58 -0400

    added array feature, variables, ages

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -2,8 +2,13 @@ Harmony Cornett 10/17/2003
 
 Conditionals_Wacky
 
-//The dog years, new research.
-//This asks, how old is your dog.  The difference (from the Sparky example from earlier videos) is that new research suggests that the actual age is multiplied by 7, UNTIL they get to a certain age.
+//The dog years, new research (Array).
+//This asks, "how old is your dog, really?".  The difference (from the Sparky example from earlier videos) is that new research suggests that the actual age is not multiplied by 7.
 
-var dogAge = ;
-var dogAgeHumanYears = dogAge * 7;
+var dogarray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+var dogsActualAge = "new Array";
+var dogAgeHumanYears = "dogAge" * 7;
+
+console.log(dogarray);
+
+
