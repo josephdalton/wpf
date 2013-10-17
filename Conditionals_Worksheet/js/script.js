91d3feaf2e0d6bbeb0a8a11204692106031b484c
Harmony Cornett <harmony@fullsail.edu>
2013-10-17 15:03:26 -0400

    finishing up the conditionals worksheet

--- Conditionals_Worksheet/js/script.js
+++ Conditionals_Worksheet/js/script.js
@@ -1,25 +1,83 @@
-//Harmony Cornett, 10/13/2013, Conditionals Worksheet
+<script type="text/javascript">
 
-//Stuff Your Face I
-//Will he qualify based on his weight?
-var weight = 264;
-//if his weight is greater than or equal to 250lbs, he will qualify.
+//SYNTAX:
 
-if(weight >= 250){
-     //if it’s greater than or equal to 250lbs, he will qualify.
-     console.log(“The competitor qualifies for the heavyweight division.”);
-}else{
-     //if it's less than 250lbs, he will not qualify.
-     console.log(“The competitor needs to gain some weight!”);
+var gasEfficiency = 23;
+var gaugeReading = 15%;
+var tankCapacity = 9;
+
+if(gaugeReading < 100%);{
+console.log ("You only have x gallons of gas in your tank, better get gas now while you can!");
+}
+
+else if(tankCapacity < 9);{
+console.log("You only have x gallons of gas in your tank, better get gas now while you can!");
+}
+
+else{
+if the condition was not equal to any of the values tested in 
+the if statement and all the else if statements, then perform 
+this action;
 }
 
 
+//HARMONY'S ExXAMPLE
+<script type="text/javascript">
 
-//Last Chance for Gas!
-//Should they stop now for gas or not?
+var gasEfficiency = 23;
+var gaugeReading = 15%;
+var tankCapacity = 9;
 
-var carMpg 6;
-var gaugeReading 27;
-var tankCapacity 8;
+x = ("tankCapacity") "+" ("gasEfficiency") "+" ("gaugeReading");
 
-making a change
+if(gaugeReading >= 100%){
+console.log("Yes, you can make it without stopping for gas!");
+}
+
+else if(gaugeReading < 100%);{
+console.log("You only have 'x' gallons of gas in your tank, better get gas now while you can!");
+}
+</script>
+
+
+
+//CHECK THE LOGIN
+//Make sure the user has the correct username and password.
+
+var username = 
+var password =
+var correctUsername =
+var correctPassword =
+
+if ("username && password === correctUsername + correctPassword = true"){
+console.log("Welcome," "+" username "+" "!");
+}
+else if ("username != correctUsername"){
+console.log("User not found. Try again.");
+}
+else ("username === correctUsername && password != correctPassword");
+console.log("Password does not match our records.");
+{
+
+
+
+//Movie Ticket Price
+//Which of the two prices the customer is eligible for?
+
+var movieTime = 3 + 4 + 5;
+var ageChild 12;
+var ageSenior  36;
+var regularPrice = 12;
+var discountPrice = 7;
+var ticketPrice = x;
+
+if (ageChild <= 10);
+}
+    console.log("The ticket price is " "+" ageChild === discountPrice === x ");
+{
+if (ageSenior >= 55);
+}
+     console.log("The ticket price is " "+" discountPrice === x");
+{
+if (
+
