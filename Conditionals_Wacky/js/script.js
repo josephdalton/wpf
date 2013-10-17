220695e2647b54f4e7c90622050c4464f44486e6
Harmony Cornett <harmony@fullsail.edu>
2013-10-17 19:42:22 -0400

    added the consol.log messages/dog ages

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -6,9 +6,26 @@ Conditionals_Wacky
 //This asks, "how old is your dog, really?".  The difference (from the Sparky example from earlier videos) is that new research suggests that the actual age is not multiplied by 7.
 
 var dogarray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-var dogsActualAge = "new Array";
-var dogAgeHumanYears = "dogAge" * 7;
+var dogsActualAge = ""
+var dogAgeHumanYears = "dogAge" 
 
-console.log(dogarray);
-
+console.log(dogarray[0]);
+console.log(dogarray[1]);
+console.log(dogarray[2]);
+console.log(dogarray[3]);
+console.log(dogarray[4]);
+console.log(dogarray[5]);
+console.log(dogarray[6]);
+console.log(dogarray[7]);
+console.log(dogarray[8]);
+console.log(dogarray[9]);
+console.log(dogarray[10]);
+console.log(dogarray[11]);
+console.log(dogarray[12]);
+console.log(dogarray[13]);
+console.log(dogarray[14]);
+console.log(dogarray[15]);
+console.log(dogarray[16]);
+console.log(dogarray[17]);
+console.log(dogarray[18]);
 
