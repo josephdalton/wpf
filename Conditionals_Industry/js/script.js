263dbe50c34773fc113c05d09abb9f31d4094eed
Harmony Cornett <harmony@fullsail.edu>
2013-10-17 20:59:15 -0400

    corrected spelling errors in the title

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -2,4 +2,4 @@ Harmony Cornett 10/17/2013
 
 //Favorit Colors and personality type!
 
-//What is your favorite coollor? What it says about your personality!
+//What is your favorite color? What it says about your personality!
