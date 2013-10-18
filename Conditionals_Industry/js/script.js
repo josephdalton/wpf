cca680c95d5d5d988a888726c989c87147617d5c
Harmony Cornett <harmony@fullsail.edu>
2013-10-17 21:21:23 -0400

    added next missing semi colan

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -3,6 +3,6 @@ Harmony Cornett 10/17/2013
 //If the football score is above or equal to 100 you win the bet!  If the score is below 100 you lose the bet! 
 
 var scoreAbove = (100);
-var scoreBelow = (99)
+var scoreBelow = (99);
 
 if insert 
