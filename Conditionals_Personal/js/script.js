c9984a92ec6ffc250aa19667840a91a6502ab2d3
Harmony Cornett <harmony@fullsail.edu>
2013-10-17 20:44:55 -0400

    fixing spacing issues

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -3,7 +3,7 @@ Harmony Cornett 10/17/2013
 //Easter baskets
 //Whoever found more than 10 eggs in their Easter basket wins the prize!
 
-var eggsInBasket =  (">= 10")
+var eggsInBasket = (">= 10")
 
 if (eggsInBasket === >10)}
 	console.log("You win the prize!")
