0508db493383ae810113a1af9a731c701d06c198
Harmony Cornett <harmony@fullsail.edu>
2013-10-17 21:32:28 -0400

    added extra variable
    to comply with standards of homework

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,9 +1,10 @@
 Harmony Cornett 10/17/2013
 
-//If the football score is above or equal to 100 you win the bet!  If the score is below 100 you lose the bet! 
+//If the football score is above or equal to 100 you win the bet!  If the score is below 100 you lose the bet! If you got neither, you did not play!
 
 var scoreAbove = (100);
 var scoreBelow = (99);
+var noScore = (0);
 
 if (scoreAbove >= 100){
 	console.log=("You win the bet!");
