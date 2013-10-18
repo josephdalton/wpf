14be4deb91d0ccc0e0f55d16756a3e06a0b7b6d7
Harmony Cornett <harmony@fullsail.edu>
2013-10-17 21:07:33 -0400

    added third variable

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -4,5 +4,6 @@ Harmony Cornett 10/17/2013
 
 //If you have 1 car or more, you can drive it to work and you don't have to take the bus.  If you have 0 cars, you have to take the bus.  If you have 0 cars and no access to bus, you need to work from home.
 
-var car
-var bus
+var car= (numberCars >= 1)
+var bus=
+var workFromHome =
