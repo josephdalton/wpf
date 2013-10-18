ce5b13de42c9f198f79052f90cf8894ed41fd93a
Harmony Cornett <harmony@fullsail.edu>
2013-10-17 21:05:45 -0400

    created car variable

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -4,6 +4,4 @@ Harmony Cornett 10/17/2013
 
 //If you have 1 car or more, you can drive it to work and you don't have to take the bus.  If you have 0 cars, you have to take the bus.  If you have 0 cars and no access to bus, you need to work from home.
 
-var brown = ()
-var red = 
-var blue =
+var car
