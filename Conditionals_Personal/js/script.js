4ec31dd4a9d084b887194ee6d4f3d7a38967c94e
Harmony Cornett <harmony@fullsail.edu>
2013-10-17 20:47:16 -0400

    removed === and added =>

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -5,7 +5,7 @@ Harmony Cornett 10/17/2013
 
 var eggsInBasket = (">= 10")
 
-if (eggsInBasket === >10)}
+if (eggsInBasket => 10)}
 	console.log("You win the prize!")
 {
 else (eggsInBasket === <10) }
