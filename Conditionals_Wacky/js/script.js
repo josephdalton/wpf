76cdd94f252218ac3641c97fc9d99ab3217b06c3
Harmony Cornett <harmony@fullsail.edu>
2013-10-17 20:19:22 -0400

    creating the new problem

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -4,3 +4,13 @@ Conditionals_Wacky
 
 //If it is snowing, there will be no school.  If it is raining, we will leave school early.  If it is neither raining or snowing, we will stay in school for the full day.
 
+var raining = ("We get out of school early!");
+var snowing = ("We don/'t have to go to school today!");
+var neither = (!= "raining" + "snowing"= "We will stay in school for the full day!");
+
+if (raining)  {
+	statements if condition is TRUE
+}
+else	{
+	statements if condition if FALSE
+}
