b003a2100292d586bf186e83f952d5281d9eaceb
Harmony Cornett <harmony@fullsail.edu>
2013-10-17 21:34:59 -0400

    finally finishing up this problem

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -10,6 +10,8 @@ if (scoreAbove >= 100){
 	console.log=("You win the bet!");
 }
 	
-else (scoreBelow <= 99)}
+else if (scoreBelow <= 99)}
 	console.log=("You lose the bet!");
 {
+else if (score !<= scoreAbove && score !>= scorebelow)
+	console.log=("You did not play!")
