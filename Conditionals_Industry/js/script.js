aba663d58e9d4894f32a7de20f302548c08b53f8
Harmony Cornett <harmony@fullsail.edu>
2013-10-17 21:18:51 -0400

    changed the scores for the new problem

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -2,7 +2,7 @@ Harmony Cornett 10/17/2013
 
 //If the football score is above or equal to INSERT you win the bet!  If the score is below INSERT you lose the bet! 
 
-var
-var
+var scoreAbove = (100)
+var scoreBelow = (0)
 
-if insert
+if insert 
