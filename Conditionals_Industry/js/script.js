d05ffbb2d9b45f043d1a5f2ce6e0b83ba2cfdbcb
Harmony Cornett <harmony@fullsail.edu>
2013-10-17 21:13:03 -0400

    changed the problem

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,6 +1,6 @@
 Harmony Cornett 10/17/2013
 
-//Favorit Colors and personality type!
+//If the football score is above or equal to INSERT you win the bet!  If the score is below INSERT you lose the bet! 
 
 //If you have 1 car or more, you can drive it to work and you don't have to take the bus.  If you have 0 cars, you have to take the bus.  If you have 0 cars and no access to bus, you need to work from home.
 
