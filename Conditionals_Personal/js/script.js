19bced9e56edd74745ae1c46bb25c2cf89cfd369
Harmony Cornett <harmony@fullsail.edu>
2013-10-17 20:44:15 -0400

    finishing up the problem

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -3,5 +3,11 @@ Harmony Cornett 10/17/2013
 //Easter baskets
 //Whoever found more than 10 eggs in their Easter basket wins the prize!
 
-var basket = ()
-var eggs = 
+var eggsInBasket =  (">= 10")
+
+if (eggsInBasket === >10)}
+	console.log("You win the prize!")
+{
+else (eggsInBasket === <10) }
+	console.log("Better luck next year!")
+{
