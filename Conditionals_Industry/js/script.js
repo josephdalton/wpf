34a8cc7b60e55bf2e54016aba7b769d29737450e
Harmony Cornett <harmony@fullsail.edu>
2013-10-17 21:11:49 -0400

    erased all variables

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -4,6 +4,3 @@ Harmony Cornett 10/17/2013
 
 //If you have 1 car or more, you can drive it to work and you don't have to take the bus.  If you have 0 cars, you have to take the bus.  If you have 0 cars and no access to bus, you need to work from home.
 
-var car= (numberCarsBus >= 1 + "You can drive your car to work!");
-var bus= (numberCarsBus >= )
-var workFromHome = (car <= 0 && bus < 1 + "You need to get a job, working from home!")
