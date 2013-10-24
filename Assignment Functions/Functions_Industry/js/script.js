812ef11e2c8e760243d6ccadf767e8a95466e3e5
Harmony Cornett <harmony@fullsail.edu>
2013-10-24 18:10:47 -0400

    finishing up the small details of the word problem

--- Assignment Functions/Functions_Industry/js/script.js
+++ Assignment Functions/Functions_Industry/js/script.js
@@ -6,6 +6,7 @@
 //This is a function with 3 parameters, and an anonymous function (to complete the requirements in the rubric)
 //Do I make enough money, with 3 paychecks, to buy a flat screen TV?
 
-function paychecks (paycheck1 paycheck2, paycheck3){
-     alert("paycheck1 = 200, paycheck2= 250, paycheck3= 189");
-	 
+function paychecks (paycheck1, paycheck2, paycheck3){
+     alert("paycheck1 = 200 " + ", paycheck2= 250," + " paycheck3= 189");
+{
+	 console.log("You do not have enough for the tv, keep saving!");
