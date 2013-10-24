07640c2748b4f513e0d84d8cbb8279ee114d071d
Harmony Cornett <harmony@fullsail.edu>
2013-10-24 18:07:48 -0400

    adding the information

--- Assignment Functions/Functions_Industry/js/script.js
+++ Assignment Functions/Functions_Industry/js/script.js
@@ -4,4 +4,8 @@
 //October 24, 2013
 
 //This is a function with 3 parameters, and an anonymous function (to complete the requirements in the rubric)
+//Do I make enough money, with 3 paychecks, to buy a flat screen TV?
 
+function paychecks (paycheck1 paycheck2, paycheck3){
+     alert("paycheck1 = 200, paycheck2= 250, paycheck3= 189");
+	 
