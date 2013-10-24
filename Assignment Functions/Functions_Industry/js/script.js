73551fb3486ad5125a5d87fee853debb28305b81
Harmony Cornett <harmony@fullsail.edu>
2013-10-24 18:01:50 -0400

    creating the description

--- Assignment Functions/Functions_Industry/js/script.js
+++ Assignment Functions/Functions_Industry/js/script.js
@@ -1,1 +1,7 @@
-This is the Javascript file
+//Harmony Cornett
+//Functions: Industry
+
+//October 24, 2013
+
+//This is a function with 3 parameters, and an anonymous function (to complete the requirements in the rubric)
+
