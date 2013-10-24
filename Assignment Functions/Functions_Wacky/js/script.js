4b2b2962286b21471e4a3b4f9864b4a94d527df8
Harmony Cornett <harmony@fullsail.edu>
2013-10-24 17:59:26 -0400

    final changes

--- Assignment Functions/Functions_Wacky/js/script.js
+++ Assignment Functions/Functions_Wacky/js/script.js
@@ -3,9 +3,9 @@
 
 //Functions Assignment (Functions_Wacky)
 
-if (candy >= 10) {
+if (candyCanes >= 10) {
 	console.log("It's a tummy-ache waiting to happen!");
-} else if (candy <= 2) {
+} else if (candyCanes <= 2) {
 	console.log("Yum!");
 }else {
 	console.log("on your way to a tummy ache")
