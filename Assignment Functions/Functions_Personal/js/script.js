f8f5254325c63d9f8bbba869fa344b9f8a8ebb7f
Harmony Cornett <harmony@fullsail.edu>
2013-10-24 17:39:12 -0400

    adding "Personal" file

--- Assignment Functions/Functions_Personal/js/script.js
+++ Assignment Functions/Functions_Personal/js/script.js
@@ -1,1 +1,9 @@
-This is the Javascript file
+//Harmony Cornett
+//October 24, 2013
+
+//Functions: (Personal)
+
+var spam = 200;
+var validMail= 150;
+	console.log("Most mail has " + (spam > 100  "spam emails than " + validMail));
+	
