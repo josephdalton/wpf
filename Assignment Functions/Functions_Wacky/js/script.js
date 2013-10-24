827595295f8b52c79b431a69c654dfddc85b9fec
Harmony Cornett <harmony@fullsail.edu>
2013-10-24 17:44:30 -0400

    adding candy

--- Assignment Functions/Functions_Wacky/js/script.js
+++ Assignment Functions/Functions_Wacky/js/script.js
@@ -1,4 +1,8 @@
-Harmony Cornett
-October 24, 2013
+//Harmony Cornett
+//October 24, 2013
 
-Functions Assignment (Functions_Wacky)
+//Functions Assignment (Functions_Wacky)
+
+if (candy == "purple") {
+	console.log
+}
