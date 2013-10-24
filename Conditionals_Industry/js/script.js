bc117a698c7b6754813de97c4c9ca4e1918f5797
josephdalton <jdalton@fullsail.com>
2013-10-24 13:14:13 -0400

    JMD conditionals commentary
    Harmony, Did you test this? There are some significant errors here in both syntax and structure.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -2,17 +2,18 @@ Harmony Cornett 10/17/2013
 
 //If the football score is above or equal to 100 you win the bet!  If the score is below 100 you lose the bet! If you got neither, you did not play!
 
+
 var scoreAbove = (100);
 var scoreBelow = (99);
 var noScore = (0);
 
 if (scoreAbove >= 100){
-	console.log=("You win the bet!");
+	console.log=("You win the bet!"); //incorrect syntax console.log('text goes here');
 }
 	
 else if (scoreBelow <= 99){
-	console.log=("You lose the bet!");
+	console.log=("You lose the bet!"); //incorrect syntax
 }
-else if (score !<= scoreAbove && score !>= scorebelow){
-	console.log=("You did not play!");
+else if (score !<= scoreAbove && score !>= scorebelow){ //conditional is incorrectly used.
+	console.log=("You did not play!"); //incorrect syntax 
 }
