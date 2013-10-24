ed03ae76b1e7d52626820bfe679de9dcb6788c27
Harmony Cornett <harmony@fullsail.edu>
2013-10-24 17:58:33 -0400

    added detailed description
    taking notes from previous comments from professor.

--- Assignment Functions/Functions_Wacky/js/script.js
+++ Assignment Functions/Functions_Wacky/js/script.js
@@ -10,3 +10,4 @@ if (candy >= 10) {
 }else {
 	console.log("on your way to a tummy ache")
 }
+ //Description:  This is an else if statement with an expression with math operators and a logical operator.  This is about candy, if the person consumes 10 or more they will have a tummy-ache.  If they consume 2 or less, they can happily enjoy it, if they consume between 3 and 9 then, they are on their way to an upset stomach.
