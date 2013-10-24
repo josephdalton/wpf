33de4c59185b4e2260f7cc9fea8ace160479f3ef
Harmony Cornett <harmony@fullsail.edu>
2013-10-24 17:40:50 -0400

    adding comments

--- Assignment Functions/Functions_Personal/js/script.js
+++ Assignment Functions/Functions_Personal/js/script.js
@@ -7,3 +7,5 @@ var spam = 200;
 var validMail= 150;
 	console.log("Most mail has " + (spam > 100  "spam emails than " + validMail));
 	
+	
+	//This is code using a "Ternary Operator"  it is saying that we get a lot of spam email compared to valid emails in our inbox.
