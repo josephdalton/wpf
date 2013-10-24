5ddf35397237a7cddcecffd72606587d54b7f644
Harmony Cornett <harmony@fullsail.edu>
2013-10-24 17:54:41 -0400

    more changes to console logs

--- Assignment Functions/Functions_Wacky/js/script.js
+++ Assignment Functions/Functions_Wacky/js/script.js
@@ -5,6 +5,8 @@
 
 if (candy >= 10) {
 	console.log("It's a tummy-ache waiting to happen!");
-} else if {
+} else if (candy <= 2) {
 	console.log("Yum!");
+}else {
+	console.log(insert something here)
 }
